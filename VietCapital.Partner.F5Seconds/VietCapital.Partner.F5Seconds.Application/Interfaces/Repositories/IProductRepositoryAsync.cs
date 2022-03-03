58ba19c48725768e2325a16b270af64169854e0b
skyrose41176 <tinhphan3119(@gmail.com>
2022-03-03 22:15:39 +0700

    ok

--- VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.Application/Interfaces/Repositories/IProductRepositoryAsync.cs
+++ VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.Application/Interfaces/Repositories/IProductRepositoryAsync.cs
@@ -9,7 +9,6 @@ namespace VietCapital.Partner.F5Seconds.Application.Interfaces.Repositories
     public interface IProductRepositoryAsync : IGenericRepositoryAsync<Product>
     {
         // Task<bool> IsUniqueBarcodeAsync(string barcode);
-
         Task<List<Product>>GetByNameAsync(string Name);
     }
 }
