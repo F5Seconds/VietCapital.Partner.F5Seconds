0e456f653692eb14a6c8307fa4588aa803f0c473
Le Anh Toan <toanla@vietbank.com.vn>
2022-03-03 15:34:12 +0700

    Initial project WebMvc

--- VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.Infrastructure.Persistence/ServiceRegistration.cs
+++ VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.Infrastructure.Persistence/ServiceRegistration.cs
@@ -2,8 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using VietCapital.Partner.F5Seconds.Application.Interfaces;
 using VietCapital.Partner.F5Seconds.Application.Interfaces.Repositories;
 using VietCapital.Partner.F5Seconds.Infrastructure.Persistence.Contexts;
@@ -16,19 +14,11 @@ namespace VietCapital.Partner.F5Seconds.Infrastructure.Persistence
     {
         public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseInMemoryDatabase("ApplicationDb"));
-            }
-            else
-            {
-                var serverVersion = new MySqlServerVersion(new Version(10, 5, 10));
-                services.AddDbContext<ApplicationDbContext>(options =>
+            var serverVersion = new MySqlServerVersion(new Version(10, 5, 10));
+            services.AddDbContext<ApplicationDbContext>(options =>
                options.UseMySql(
                    configuration.GetConnectionString("DefaultConnection"), serverVersion,
                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-            }
             #region Repositories
             services.AddTransient(typeof(IGenericRepositoryAsync<>), typeof(GenericRepositoryAsync<>));
             services.AddTransient<IProductRepositoryAsync, ProductRepositoryAsync>();
