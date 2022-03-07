4017649f005c7cfa012e8ae18ec1b50c6eb9e5ba
Le Anh Toan <toanla@vietbank.com.vn>
2022-03-07 08:58:48 +0700

    Tích hợp Identity page và pagelist

---
+++ VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.WebMvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VietCapital.Partner.F5Seconds.Infrastructure.Identity.Contexts;
+using VietCapital.Partner.F5Seconds.Infrastructure.Identity.Models;
+
+[assembly: HostingStartup(typeof(VietCapital.Partner.F5Seconds.WebMvc.Areas.Identity.IdentityHostingStartup))]
+namespace VietCapital.Partner.F5Seconds.WebMvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.WebMvc/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VietCapital.Partner.F5Seconds.WebMvc.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
