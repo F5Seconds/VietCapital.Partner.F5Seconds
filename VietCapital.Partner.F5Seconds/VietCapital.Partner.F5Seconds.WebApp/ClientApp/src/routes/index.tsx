d194c8a2a228085ca4bb2295b26ec5e9ac9e3bd6
luankat0306 <votriluan99@gmail.com>
2022-03-10 20:55:54 +0700

    thêm đăng nhập

--- VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.WebApp/ClientApp/src/routes/index.tsx
+++ VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.WebApp/ClientApp/src/routes/index.tsx
@@ -1,14 +1,29 @@
-import {Outlet, RouteObject} from 'react-router-dom';
+import {FC} from 'react';
+import {Outlet, RouteObject, Navigate} from 'react-router-dom';
 import {NotFound} from '../components';
 import MainLayout from '../layouts';
 import LoginPage from '../modules/auth/login';
 import DanhSachUser from '../modules/quan-ly-user/danh-sach-user';
 import PhanQuyenUser from '../modules/quan-ly-user/phan-quyen-user';
+import {useAppSelector} from '../redux/hooks';
+import {selectJWT} from '../redux/slice/auth';
 
+const Auth: FC<{children: React.ReactElement}> = ({children}) => {
+  const jwt = useAppSelector(selectJWT);
+
+  if (!jwt) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
 export const routes: RouteObject[] = [
   {
     path: '/',
-    element: <MainLayout />,
+    element: (
+      <Auth>
+        <MainLayout />
+      </Auth>
+    ),
     children: [
       {index: true, element: <div>home</div>},
       {
