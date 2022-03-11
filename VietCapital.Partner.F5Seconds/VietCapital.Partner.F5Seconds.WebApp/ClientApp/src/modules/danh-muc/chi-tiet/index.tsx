5667a1f2ec89b76a83a3fab7184da7837b6f0a74
Vuongnguyen1705 <vuongnguyen.170599@gmail.com>
2022-03-11 22:37:08 +0700

    fix giao dien chi tiet danh muc

--- VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.WebApp/ClientApp/src/modules/danh-muc/chi-tiet/index.tsx
+++ VietCapital.Partner.F5Seconds/VietCapital.Partner.F5Seconds.WebApp/ClientApp/src/modules/danh-muc/chi-tiet/index.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {Button, Grid, Stack} from '@mui/material';
+import {Button, Card, Grid, Stack} from '@mui/material';
 import React, {useEffect} from 'react';
 import {useForm} from 'react-hook-form';
 import {useParams} from 'react-router-dom';
+import {CardBase} from '../../../components/base';
 import {CheckboxField, InputField} from '../../../components/hook-form';
 import Header from '../../../layouts/Header';
 import {categoryService} from '../../../services';
@@ -35,22 +36,29 @@ const DanhMucSanPhamPage = () => {
   return (
     <div>
       <Header title="Chi tiết danh mục" />
-      <Grid container sx={{padding: 2}} spacing={2}>
-        <Grid item xs={12} md={6} lg={4}>
-          <InputField form={form} name="name" label="Tên sản phẩm" />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <InputField form={form} name="image" label="Hình ảnh" />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <CheckboxField form={form} name="status" label="Trạng thái" />
-        </Grid>
-        <Stack direction="row" justifyContent="flex-end" margin={2}>
-          <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
-            {id ? 'Cập nhật' : 'Thêm danh mục'}
-          </Button>
-        </Stack>
-      </Grid>
+      <div style={{padding: 16}}>
+        <CardBase
+          actions={
+            <Stack direction="row" justifyContent="flex-end" margin={2}>
+              <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
+                {id ? 'Cập nhật' : 'Thêm danh mục'}
+              </Button>
+            </Stack>
+          }
+        >
+          <Grid container sx={{padding: 2}} spacing={2}>
+            <Grid item xs={12} md={6} lg={4}>
+              <InputField form={form} name="name" label="Tên sản phẩm" />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <InputField form={form} name="image" label="Hình ảnh" />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <CheckboxField form={form} name="status" label="Trạng thái" />
+            </Grid>
+          </Grid>
+        </CardBase>
+      </div>
     </div>
   );
 };
